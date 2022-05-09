009f9c3e594020df1b7d59eedffc77670f489781
Kneelawk <kneelawk@gmail.com>
2022-05-09 01:18:26 -0700

    Make mod class use api for accessing controller

--- src/main/java/com/kneelawk/graphlib/GraphLibMod.java
+++ src/main/java/com/kneelawk/graphlib/GraphLibMod.java
@@ -1,6 +1,5 @@
 package com.kneelawk.graphlib;
 
-import com.kneelawk.graphlib.mixin.api.StorageHelper;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerChunkEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
@@ -12,9 +11,9 @@ public class GraphLibMod implements ModInitializer {
         GraphLib.register();
 
         ServerChunkEvents.CHUNK_LOAD.register(
-                (world, chunk) -> StorageHelper.getController(world).onWorldChunkLoad(chunk.getPos()));
+                (world, chunk) -> GraphLib.getController(world).onWorldChunkLoad(chunk.getPos()));
         ServerChunkEvents.CHUNK_UNLOAD.register(
-                (world, chunk) -> StorageHelper.getController(world).onWorldChunkUnload(chunk.getPos()));
-        ServerTickEvents.END_WORLD_TICK.register(world -> StorageHelper.getController(world).tick());
+                (world, chunk) -> GraphLib.getController(world).onWorldChunkUnload(chunk.getPos()));
+        ServerTickEvents.END_WORLD_TICK.register(world -> GraphLib.getController(world).tick());
     }
 }
