e0227f24c1a7b9e1995ce85b08ff6a2070b7d1de
Kneelawk <kneelawk@gmail.com>
2022-05-10 16:51:24 -0700

    Add simple graph tests

---
+++ src/test/java/com/kneelawk/graphlib/graph/struct/GraphMergeTests.java
@@ -0,0 +1,28 @@
+package com.kneelawk.graphlib.graph.struct;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GraphMergeTests {
+    @Test
+    public void simpleMergeTest() {
+        Graph<String> graphA = new Graph<>();
+        Graph<String> graphB = new Graph<>();
+
+        var a = graphA.add("A");
+        var b = graphB.add("B");
+        var c = graphB.add("C");
+        var link = graphB.link(b, c);
+
+        graphA.join(graphB);
+
+        assertEquals("Graph A should have 3 nodes.", 3, graphA.size());
+        assertEquals("Graph B should have 0 nodes.", 0, graphB.size());
+
+        assertTrue("Graph A should contain A, B, and C.",
+                graphA.contains(a) && graphA.contains(b) && graphA.contains(c));
+        assertTrue("B and C should stay linked.", b.connections().contains(link) && c.connections().contains(link));
+    }
+}
---
+++ src/test/java/com/kneelawk/graphlib/graph/struct/GraphSplitTests.java
@@ -0,0 +1,32 @@
+package com.kneelawk.graphlib.graph.struct;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GraphSplitTests {
+    @Test
+    public void simpleSplitTest() {
+        Graph<String> graph = new Graph<>();
+
+        var a = graph.add("A");
+        var b = graph.add("B");
+        graph.link(a, b);
+
+        var c = graph.add("C");
+
+        var newGraphs = graph.split();
+
+        assertEquals("There should be one new graph.", 1, newGraphs.size());
+
+        Graph<String> newGraph = newGraphs.get(0);
+
+        assertTrue("Either the new graph or the old graph should contain C.",
+                newGraph.contains(c) || graph.contains(c));
+        assertTrue("Either the new graph or the old graph should contain both A and B.",
+                (newGraph.contains(a) && newGraph.contains(b)) || (graph.contains(a) && graph.contains(b)));
+
+        assertFalse("The same graph should not contain both A and C.",
+                (newGraph.contains(a) && newGraph.contains(c)) || (graph.contains(a) && graph.contains(c)));
+    }
+}
