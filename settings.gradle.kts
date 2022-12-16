55e623de3c6c30dd284be4ac4826b328a38e77de
Kneelawk <kneelawk@gmail.com>
2022-12-15 16:37:19 -0800

    Update toolchain to 1.19.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,8 +13,6 @@ pluginManagement {
         id("fabric-loom") version loom_version
         val loom_quiltflower_version: String by settings
         id("io.github.juuxel.loom-quiltflower") version loom_quiltflower_version
-        val quilt_mappings_on_loom_version: String by settings
-        id("org.quiltmc.quilt-mappings-on-loom") version quilt_mappings_on_loom_version
     }
 }
 
