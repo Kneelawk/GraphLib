c8588275a5b1bef12502505eb22f4aaae71069e1
Kneelawk <kneelawk@gmail.com>
2023-05-20 19:35:49 -0700

    Actually call onUnload()

--- src/main/java/com/kneelawk/graphlib/impl/graph/simple/SimpleBlockGraph.java
+++ src/main/java/com/kneelawk/graphlib/impl/graph/simple/SimpleBlockGraph.java
@@ -472,4 +472,10 @@ public class SimpleBlockGraph implements BlockGraph {
             return List.of();
         }
     }
+
+    void onUnload() {
+        for (var node : graph) {
+            node.value().getNode().onUnload();
+        }
+    }
 }
