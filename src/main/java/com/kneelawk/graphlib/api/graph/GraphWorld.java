f94b9f2b4dbf9c0f93303c8f26faf4626033bc97
Kneelawk <kneelawk@gmail.com>
2023-05-31 21:09:19 -0700

    Add key-less connect & disconnect methods

--- src/main/java/com/kneelawk/graphlib/api/graph/GraphWorld.java
+++ src/main/java/com/kneelawk/graphlib/api/graph/GraphWorld.java
@@ -9,6 +9,7 @@ import net.minecraft.util.math.BlockPos;
 
 import com.kneelawk.graphlib.api.graph.user.LinkEntityFactory;
 import com.kneelawk.graphlib.api.graph.user.LinkKey;
+import com.kneelawk.graphlib.api.util.EmptyLinkKey;
 import com.kneelawk.graphlib.api.util.NodePos;
 import com.kneelawk.graphlib.api.util.SidedPos;
 
@@ -16,6 +17,20 @@ import com.kneelawk.graphlib.api.util.SidedPos;
  * Holds and manages all block graphs for a given world.
  */
 public interface GraphWorld extends GraphView {
+
+    /**
+     * Connects two nodes to each other.
+     * <p>
+     * Note: in order for manually connected links to not be removed when the connected nodes are updated,
+     * {@link LinkKey#isAutomaticRemoval(LinkContext)} should return <code>false</code> for the given key.
+     *
+     * @param a the first node to be connected.
+     * @param b the second node to be connected.
+     * @return the link created, or <code>null</code> if no link could be created.
+     */
+    default @Nullable LinkHolder<LinkKey> connectNodes(NodePos a, NodePos b) {
+        return connectNodes(a, b, EmptyLinkKey.INSTANCE, EmptyLinkKey.INSTANCE::createLinkEntity);
+    }
 
     /**
      * Connects two nodes to each other.
@@ -45,6 +60,17 @@ public interface GraphWorld extends GraphView {
      * @return the link created, or <code>null</code> if no link could be created.
      */
     @Nullable LinkHolder<LinkKey> connectNodes(NodePos a, NodePos b, LinkKey key, LinkEntityFactory entityFactory);
+
+    /**
+     * Disconnects two nodes from each other.
+     *
+     * @param a the first node to be disconnected.
+     * @param b the second node to be disconnected.
+     * @return <code>true</code> if a link was actually removed, <code>false</code> otherwise.
+     */
+    default boolean disconnectNodes(NodePos a, NodePos b) {
+        return disconnectNodes(a, b, EmptyLinkKey.INSTANCE);
+    }
 
     /**
      * Disconnects two nodes from each other.
