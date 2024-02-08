0a02ab828668ce1ea369cae880c39e722b9a8216
Kneelawk <kneelawk@gmail.com>
2024-02-07 19:01:19 -0800

    Begin making debugrender multiplatform

---
+++ modules/debugrender-xplat/build.gradle.kts
@@ -0,0 +1,146 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Kneelawk.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+plugins {
+    `maven-publish`
+    id("architectury-plugin")
+    id("dev.architectury.loom")
+    id("com.kneelawk.versioning")
+}
+
+val maven_group: String by project
+group = maven_group
+
+val archives_base_name: String by project
+base {
+    archivesName.set("$archives_base_name-${project.name}-intermediary")
+}
+
+java.docsDir.set(rootProject.layout.buildDirectory.map { it.dir("docs").dir("graphlib-${project.name}") })
+
+architectury {
+    val enabled_platforms: String by project
+    common(enabled_platforms.split(','))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.quiltmc.org/repository/release") { name = "Quilt" }
+
+    mavenLocal()
+}
+
+dependencies {
+    val minecraft_version: String by project
+    minecraft("com.mojang:minecraft:$minecraft_version")
+    val quilt_mappings: String by project
+    mappings("org.quiltmc:quilt-mappings:$minecraft_version+build.$quilt_mappings:intermediary-v2")
+
+    // Using modCompileOnly & modLocalRuntime so that these dependencies don't get brought into any projects that depend
+    // on this one.
+
+    // Fabric Loader
+    val fabric_loader_version: String by project
+    modCompileOnly("net.fabricmc:fabric-loader:$fabric_loader_version")
+    modLocalRuntime("net.fabricmc:fabric-loader:$fabric_loader_version")
+
+    testImplementation("junit:junit:4.13.2")
+}
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+
+        filesMatching("quilt.mod.json") {
+            expand(mapOf("version" to project.version))
+        }
+        filesMatching("fabric.mod.json") {
+            expand(mapOf("version" to project.version))
+        }
+    }
+
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        withJavadocJar()
+        withSourcesJar()
+    }
+
+    jar {
+        from(rootProject.file("LICENSE")) {
+            rename { "${it}_${archives_base_name}" }
+        }
+    }
+
+    javadoc {
+        exclude("com/kneelawk/graphlib/debugrender/impl")
+
+//        val minecraft_version: String by project
+//        val quilt_mappings: String by project
+        val jetbrains_annotations_version: String by project
+//        val lns_version: String by project
+        (options as? StandardJavadocDocletOptions)?.links = listOf(
+//            "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-mappings/$minecraft_version+build.$quilt_mappings/quilt-mappings-$minecraft_version+build.$quilt_mappings-javadoc.jar/",
+            "https://javadoc.io/doc/org.jetbrains/annotations/${jetbrains_annotations_version}/",
+//            "https://alexiil.uk/javadoc/libnetworkstack/${lns_version}/"
+        )
+
+        options.optionFiles(rootProject.file("javadoc-options.txt"))
+    }
+
+    test {
+        useJUnit()
+    }
+
+    afterEvaluate {
+        named("genSources") {
+            setDependsOn(listOf("genSourcesWithVineflower"))
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "${project.name}-intermediary"
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        if (System.getenv("PUBLISH_REPO") != null) {
+            maven {
+                name = "publishRepo"
+                url = uri(rootProject.file(System.getenv("PUBLISH_REPO")))
+            }
+        }
+    }
+}
