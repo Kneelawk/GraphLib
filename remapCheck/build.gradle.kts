1571ff9f010015dd4d449cb84df59608b8d0d06e
Kneelawk <kneelawk@gmail.com>
2024-05-04 00:21:56 -0700

    Add remapcheck

---
+++ remapCheck/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins { 
+    id("dev.architectury.loom")
+    id("com.kneelawk.remapcheck")
+}
+
+remapCheck {
+    val minecraft_version: String by project
+    val yarn_version: String by project
+    applyTargetMapping("net.fabricmc:yarn:$minecraft_version+build.$yarn_version:v2")
+    checkRemap {
+        targetProject(":core-fabric")
+        taskNameBase("core")
+    }
+    checkRemap {
+        targetProject(":debugrender-fabric")
+        taskNameBase("debugrender")
+    }
+    checkRemap {
+        targetProject(":syncing-core-fabric")
+        taskNameBase("syncing-core")
+    }
+    checkRemap {
+        targetProject(":syncing-knet-fabric")
+        taskNameBase("syncing-knet")
+    }
+    checkRemap {
+        targetProject(":syncing-lns")
+        taskNameBase("syncing-lns")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,8 @@ pluginManagement {
     plugins {
         val architectury_loom_version: String by settings
         id("dev.architectury.loom") version architectury_loom_version
+        val remapcheck_version: String by settings
+        id("com.kneelawk.remapcheck") version remapcheck_version
     }
 }
 
@@ -78,3 +80,5 @@ example("multiblock-lamps", "xplat", "fabric", "neoforge")
 javadoc("xplat")
 javadoc("fabric")
 javadoc("neoforge")
+
+include(":remapCheck")
